9e67fd1fa9a07d30545d6a158bf7b5267bf4c89d
ConstantineVac <108877593+ConstantineVac@users.noreply.github.com>
2023-10-17 17:05:50 +0300

    Update Meeting.java
    Convenience Methods for Meetings Class

--- school-app-pro/src/main/java/grauebcf/schoolapppro/model/Meeting.java
+++ school-app-pro/src/main/java/grauebcf/schoolapppro/model/Meeting.java
@@ -34,4 +34,29 @@ public class Meeting {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "student_id")
     private Student student;
+
+    // Convenience Methods
+    public void setTeacher(Teacher teacher) {
+        if (this.teacher != null && !this.teacher.equals(teacher)) {
+            this.teacher.getMeetings().remove(this);
+        }
+
+        this.teacher = teacher;
+
+        if (teacher != null && !teacher.getMeetings().contains(this)) {
+            teacher.getMeetings().add(this);
+        }
+    }
+
+    public void setStudent(Student student) {
+        if (this.student != null && !this.student.equals(student)) {
+            this.student.getMeetings().remove(this);
+        }
+
+        this.student = student;
+
+        if (student != null && !student.getMeetings().contains(this)) {
+            student.getMeetings().add(this);
+        }
+    }
 }
