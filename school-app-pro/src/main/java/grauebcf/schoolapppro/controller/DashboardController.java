5987997d0b82dcd702437ffb0fd37cf06d0a6acb
ConstantineVac <108877593+ConstantineVac@users.noreply.github.com>
2023-11-25 04:51:44 +0200

    Update DashboardController.java

--- school-app-pro/src/main/java/grauebcf/schoolapppro/controller/DashboardController.java
+++ school-app-pro/src/main/java/grauebcf/schoolapppro/controller/DashboardController.java
@@ -1,10 +1,9 @@
 package grauebcf.schoolapppro.controller;
 
-import grauebcf.schoolapppro.service.CustomUserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.security.Principal;
 
@@ -14,8 +13,7 @@ public class DashboardController {
     @GetMapping("/dashboard")
     public String dashboard(Principal principal, Model model) {
         if (principal != null) {
-            String username = principal.getName(); // This gets the username (email) from UserDetails
-            // Use username in your logic or add it to the model
+            String username = principal.getName(); 
             model.addAttribute("username", username);
         }
         return "dashboard";
